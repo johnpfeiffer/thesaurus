87545e4cce37831c87eda13bd2c5705efde6ce39
John Pfeiffer <foupfeiffer@gmail.com>
2024-10-13 20:45:31 -0700

    Start a basic React app for a tiny thesaurus
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/johnpfeiffer/thesaurus?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Thesaurus from './components/Thesaurus';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Thesaurus</h1>
+      </header>
+      <Thesaurus />
+    </div>
+  );
+}
+
+export default App;
